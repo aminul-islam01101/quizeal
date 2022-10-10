66320684e16b5e88a50c884007d60370fdf16cef
aminulislam01101 <aminul.islam01101@gmail.com>
2022-10-10 21:34:21 +0600

    First commit-Folder setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+    console.log('welcome');
+
+    return (
+        <div>
+            <p>Hello world</p>
+            <button type="button" className="btn bg-red-500">
+                Button
+            </button>
+        </div>
+    );
+}
+
+export default App;
