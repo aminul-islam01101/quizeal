a077a8112b61b0a6a2e2fdf64283a9d38ac0a0bb
aminulislam01101 <aminul.islam01101@gmail.com>
2022-10-12 04:59:46 +0600

    footer added

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -1,4 +1,5 @@
 import { Outlet, useLoaderData } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 import Navbar from '../components/Navbar';
 import { QuizContext } from '../Contexts/QuizContext';
@@ -9,9 +10,16 @@ const Root = () => {
     return (
         // eslint-disable-next-line react/jsx-no-constructed-context-values
         <QuizContext.Provider value={quizTopics}>
-            <div className="min-h-screen container">
-                <Navbar />
-                <Outlet />
+            <div className="flex flex-col justify-between min-h-screen ">
+                <div >
+                    <Navbar />
+                    <div className="container">
+                        <Outlet />
+                    </div>
+                </div>
+                <div className="">
+                    <Footer />
+                </div>
             </div>
         </QuizContext.Provider>
     );
